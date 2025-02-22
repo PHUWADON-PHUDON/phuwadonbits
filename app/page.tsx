73197b0b6ca144b0fa37e9afcfdd8057b3b50adf
PLUG <ppuwdon@gmail.com>
2025-02-22 15:05:03 +0700

    update 1.1

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
+"use client"
 import dynamic from "next/dynamic";
 const SplitText = dynamic(() => import('@/components/Splittext'), {
+    ssr: false,
     loading: () => <h1 className="text-white text-[100px]">Loading...</h1>
 });
 
